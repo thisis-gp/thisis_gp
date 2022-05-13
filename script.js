c363df6c09da70aff2a7ea43a2fd15a94280b707
GURU PRIYAN <am400718@gmail.com>
2022-05-13 23:44:48 +0530

    Final site

---
+++ script.js
@@ -0,0 +1,89 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+
+
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    var navWidth = $('nav').outerWidth(true);
+    console.log(navWidth);
+    $('section').css({
+        'margin-left': navWidth + 'px',
+    })
+    $('#sidebarMenu').toggle();
+    $("#closeBtn").show();
+    $('#navBtn').hide()
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    $('section').css({
+        'margin-left': '0px',
+    })
+    $('#sidebarMenu').toggle();
+    $("#closeBtn").hide();
+    $('#navBtn').show();
+  }
+});
